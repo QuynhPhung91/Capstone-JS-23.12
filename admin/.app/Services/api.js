8676ffbdb13b4e3e7be30e6ddb35ec642025e896
Quynh Phung <139933051+QuynhPhung91@users.noreply.github.com>
2024-12-22 19:37:42 +0700

    add index.html, src, js, img, css, admin

---
+++ admin/.app/Services/api.js
@@ -0,0 +1,40 @@
+class API {
+  fetchData() {
+    const promise = axios({
+      url: "https://67563d2e11ce847c992c3910.mockapi.io/api/Products",
+      method: "GET",
+    });
+    return promise;
+  }
+  deleteDataById(id) {
+    const promise = axios({
+      url: `https://67563d2e11ce847c992c3910.mockapi.io/api/Products/${id}`,
+      method: "DELETE",
+    });
+    return promise;
+  }
+  addData(product) {
+    const promise = axios({
+      url: `https://67563d2e11ce847c992c3910.mockapi.io/api/Products`,
+      method: "POST",
+      data: product,
+    });
+    return promise;
+  }
+  getDataById(id) {
+    const promise = axios({
+      url: `https://67563d2e11ce847c992c3910.mockapi.io/api/Products/${id}`,
+      method: "GET",
+    });
+    return promise;
+  }
+  updateData(product) {
+    const promise = axios({
+      url: `https://67563d2e11ce847c992c3910.mockapi.io/api/Products/${product.id}`,
+      method: "PUT",
+      data: product,
+    });
+    return promise;
+  }
+}
+export default new API();
